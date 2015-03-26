997862257659f46d3d2276ad9cf9049e97d5c93c
jiwu4742 <jiwu4742@uni.sydney.edu.au>
2015-03-27 01:55:42 +1100

    changing schema for user story B

--- schema.sql
+++ schema.sql
@@ -3,6 +3,10 @@ create table entries (
   id integer primary key autoincrement,
   title text not null,
   text text not null,
-  username text not null
+  username text not null,
+  sdate text not null,
+  stime text not null,
+  edate text not null,
+  etime text not null
 );
 
