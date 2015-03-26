7dbc88ec1f4c0f36e6d238a7672e669e00216010
jiwu4742 <jiwu4742@uni.sydney.edu.au>
2015-03-26 15:19:54 +1100

    bugfix for showing entries with username

--- flaskr.py
+++ flaskr.py
@@ -38,7 +38,7 @@ def teardown_request(exception):
 @app.route('/')
 def show_entries():
     cur = g.db.execute('select title, text, username from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    entries = [dict(title=row[0], text=row[1], username=row[2]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
