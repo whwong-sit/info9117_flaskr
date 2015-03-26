30931a857411a6e84f5d20e056dac71da468b769
jiwu4742 <jiwu4742@uni.sydney.edu.au>
2015-03-27 02:11:10 +1100

    1st attempt of implemeting B

--- flaskr.py
+++ flaskr.py
@@ -37,16 +37,16 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text, username from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1], username=row[2]) for row in cur.fetchall()]
+    cur = g.db.execute('select title, text, username, sdate, stime, edate, etime from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1], username=row[2], sdate=row[3], stime=row[4], edate=row[5], etime=row[6]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entries (title, text, username) values (?,?,?)',
-                 [request.form['title'], request.form['text'], session['username']])
+    g.db.execute('insert into entries (title, text, username, sdate, stime, edate, etime) values (?,?,?,?,?,?,?)',
+                 [request.form['title'], request.form['text'], session['username'], request.form['sdate'], request.form['stime'], request.form['edate'], request.form['etime']])
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
