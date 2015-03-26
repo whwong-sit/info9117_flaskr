c2134ddf97173b08c1300261e66463bf5c91c315
jiwu4742 <jiwu4742@uni.sydney.edu.au>
2015-03-26 15:14:09 +1100

    implemented adding entries with username

--- flaskr.py
+++ flaskr.py
@@ -37,7 +37,7 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
+    cur = g.db.execute('select title, text, username from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
@@ -45,8 +45,8 @@ def show_entries():
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entries (title, text) values (?,?)',
-                 [request.form['title'], request.form['text']])
+    g.db.execute('insert into entries (title, text, username) values (?,?,?)',
+                 [request.form['title'], request.form['text'], request.form['username']])
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
