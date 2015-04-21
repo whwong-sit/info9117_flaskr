01b9f025e7868b174ea3fe91aded2d4791bbdfe1
Thomas Porcaro <tpor9095@uni.sydney.edu.au>
2015-04-21 23:24:58 +1000

    Added a features folder for use with behave testing
    ``behave'' is a beautiful Python module for writing acceptance tests
    using what amounts to natural language (actually it's called Gherkin).
    
    Please please please check out
    
      http://pythonhosted.org/behave/index.html
    
    as we will be using is for the rest of the semester.
    
    Once you have written the acceptance tests in Gherkin you link the
    Gherkin statements up to actual code in the ``steps'' files, full of
    @given, @when and @then decorators, associated Gherkin statements and
    code.
    
    The environment.py file is used to set up the environment for behave;
    you can have it set up environments for each scenario, for the whole
    ``shooting match'' (as they are fond of saying in the behave
    documentation for some reason), even for certain steps that have tags
    you set.
    
    As it stands, this commit represents only a framework; the steps
    files(s) still needs the actual code inside the functions.  There also
    remains some ambiguity about what exactly the requirements are (for
    example, should users be able to edit their username?), but this is to
    be expected.  This sprint consultation will be limited, but in the
    next sprint hopefully such issues can be clarified.

---
+++ features/steps/steps.py
@@ -0,0 +1,37 @@
+from behave import *
+
+@given(u'the User is logged in')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the User is logged in')
+
+@when(u'the User navigates to the web interface')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the User navigates to the web interface')
+
+@then(u'account details are displayed')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then account details are displayed')
+
+@given(u'the User navigates to the web interface')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the User navigates to the web interface')
+
+@when(u'the User clicks "change username"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the User clicks "change username"')
+
+@then(u'the User is able to edit username')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the User is able to edit username')
+
+@then(u'the User is able to commit the new value successfully')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the User is able to commit the new value successfully')
+
+@when(u'the User clicks "change password"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the User clicks "change password"')
+
+@then(u'the User is able to edit password')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the User is able to edit password')
