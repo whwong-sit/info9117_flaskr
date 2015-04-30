73e3b1dfccc33652d29dad1489eabeb8b5bffce3
Thomas Porcaro <tpor9095@uni.sydney.edu.au>
2015-04-30 18:28:06 +1000

    Cleaned up meterage.py
    I have cleaned up some of the logic in meterage.py, and added some
    docstrings:
    
    https://www.python.org/dev/peps/pep-0257/
    
    I would ask that everyone try to include docstrings, as it makes it
    far clearer what the functions you write do for readers.
    
    I have also added a couple of TODOs; in PyCharm, if your comment
    starts with TODO then it gets picked up automatically if you go to
    View > Tool Windows > TODO.  This is really useful for seeing the
    minutiae of what needs to be done.  In this case, I said that some of
    the funcitons rely on username being unique, and this is not
    necessarily the case as far as I can tell.  Someone with a bit more
    database experience needs to please remedy this.

--- meterage.py
+++ meterage.py
@@ -2,16 +2,12 @@ from contextlib import closing
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
+# from os.path import isfile
 
 # configuration
 DATABASE = 'db/flask.db'
 DEBUG = True
 SECRET_KEY = 'development key'
-
-#USERNAME = 'admin'
-#PASSWORD = 'default'
-#USERS = {'admin':'default','adam':'alpha','bob':'bravo','cat':'charlie'}
-#USERS = {'admin': 'default', 'jim': 'bean', 'spock': 'vulcan'}
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -20,10 +16,16 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 def connect_db():
+    """
+    Make a connection to the database specified in the config.
+    """
     return sqlite3.connect(app.config['DATABASE'])
 
 
 def init_db():
+    """
+    For initialising the database using schemal.sql.  This is usually called manually.
+    """
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
@@ -44,17 +46,28 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
+    """
+    Get all the information required in show_entries.html from the database and pipe it
+    into show_entries.html
+    """
     cur = g.db.execute('select title, text, username, sdate, stime, edate, etime from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1], username=row[2], sdate=row[3], stime=row[4], edate=row[5], etime=row[6]) for row in cur.fetchall()]
+    entries = [dict(title=row[0], text=row[1], username=row[2], sdate=row[3], stime=row[4], edate=row[5], etime=row[6])
+               for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
 
 @app.route('/add', methods=['POST'])
 def add_entry():
+    """
+    Make a new entry, including title, text, username, start time and date (stime, sdate) and
+    end time and date (etime, edate)
+    """
     if not session.get('logged_in'):
+        # If someone tries to access the page without being logged in
         abort(401)
     g.db.execute('insert into entries (title, text, username, sdate, stime, edate, etime) values (?,?,?,?,?,?,?)',
-                 [request.form['title'], request.form['text'], session['username'], request.form['sdate'], request.form['stime'], request.form['edate'], request.form['etime']])
+                 [request.form['title'], request.form['text'], session['username'], request.form['sdate'],
+                  request.form['stime'], request.form['edate'], request.form['etime']])
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
@@ -62,62 +75,76 @@ def add_entry():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    Check to see if the entered username is in the database.  If no, present an error.
+    If the username is present in the database, check that the given password corresponds
+    to the given username.  If so, log in, otherwise present an error.
+    """
     error = None
     if request.method == 'POST':
-        cursor = g.db.execute('select username, password from userPassword where username=?', [request.form['username']])
-        row = cursor.fetchone()
+        # get the username-password combination from the database
+        cur = g.db.execute('select username, password from userPassword where username=?', [request.form['username']])
+        row = cur.fetchone()
+
         if row is not None:
-            user = {'username':row[0], 'password':row[1]}
-            #print user
-            if user['username'] is None:
-                error = 'Invalid username'
-            elif user['password'] is None or request.form['password']=='':
+            # if the user is found
+            user = {'username': row[0], 'password': row[1]}
+            if request.form['password'] != user['password']:
                 error = 'Invalid password'
-            elif user is not None:
-                if request.form['password']!=user['password']:
-                    error = 'Invalid password'
-                elif request.form['password']==user['password']:
-                    session['logged_in'] = True
-                    session['username'] = user['username']
-                    flash('You were logged in')
-                    return redirect(url_for('show_entries'))
+            else:
+                session['logged_in'] = True
+                session['username'] = user['username']
+                flash('You were logged in')
+                return redirect(url_for('show_entries'))
+        else:
+            # TODO username needs to be made unique in the database,
+            # TODO otherwise this method will malfunction
+            error = 'Invalid username'
     return render_template('login.html', error=error)
+
 
 @app.route('/logout')
 def logout():
-	session.pop('logged_in', None)
-	flash('You were logged out')
-	return redirect(url_for('show_entries'))
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
 
-	
-@app.route('/change_password',methods=['GET','POST'])
+
+@app.route('/change_password', methods=['GET', 'POST'])
 def change_password():
+    """
+    Allows the admin to change the password of a user.  It searches the database for the user specified.
+    If that user is present, it checks that the new password is not empty and that the new password
+    corresponds to the confirmation password given.  If all of this holds, then the user's password
+    is updated.
+    """
     error = None
     if not session.get('logged_in'):
+        # If someone tries to access the page without being logged in.
         abort(401)
     if request.method == 'POST':
-        user=request.form['username']
-        passwd=request.form['password']
-        confirm_passwd=request.form['confirm_password']
-        if user is None or user=='':
-            error = 'Empty username'
-        elif passwd is None or passwd=='':
+        cursor = g.db.execute('select username from userPassword where username=?', [request.form['username']])
+        row = cursor.fetchone()
+
+        if row is None:
+            error = 'User does not exist'
+        elif request.form['password'] is None or request.form['password'] == '':
             error = 'Empty password'
-        elif passwd!=confirm_passwd:
+        elif request.form['password'] != request.form['confirm_password']:
             error = 'Please enter same password twice'
         else:
-            cursor = g.db.execute('select username from userPassword where username=?', [request.form['username']])
-            row = cursor.fetchone()
-            if row is None:
-                error = 'User does not exist'
-                return render_template('change_password.html',error=error)
-            else:
-                g.db.execute('update userPassword set password=? where username =?', [request.form['password'],request.form['username']])
+            g.db.execute('update userPassword set password=? where username =?',
+                             [request.form['password'], request.form['username']])
             g.db.commit()
             flash('Successfully changed user password')
             return render_template('change_password.html', success='Successfully changed password')
+
     return render_template('change_password.html', error=error)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    # create the database if it's not already there.
+    # if not isfile(str(app.config['DATABASE'])):
+    #     app.logger.debug('creating database')
+    #     init_db()
+    app.run()
