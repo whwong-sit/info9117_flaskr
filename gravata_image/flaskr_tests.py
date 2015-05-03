acc10c3db017132003819090758bb92849b1c0ca
daisy200029 <daisy22229999@gmail.com>
2015-05-03 22:44:34 +1000

    Add gravata_image folder

---
+++ gravata_image/flaskr_tests.py
@@ -0,0 +1,86 @@
+import os
+import flaskr
+import unittest
+import tempfile
+import hashlib
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.config['TESTING'] = True
+        self.app = flaskr.app.test_client()
+        flaskr.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink( flaskr.app.config['DATABASE'] )
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert 'No entries here yet' in rv.data
+
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_multiple_login_logout(self):
+        # Test admin login
+        rv = self.login('admin', 'default')
+        assert 'You were logged in' in rv.data
+        rv = self.logout()
+        assert 'You were logged out' in rv.data
+
+        # Test Jim login
+        rv = self.login('jim', 'bean')
+        assert 'You were logged in' in rv.data
+        rv = self.logout()
+        assert 'You were logged out' in rv.data
+        # Test Spock login
+        rv = self.login('spock', 'vulcan')
+        assert 'You were logged in' in rv.data
+        rv = self.logout()
+        assert 'You were logged out' in rv.data
+
+
+        # Test non-recognised users
+        rv = self.login('adminx', 'default')
+        assert 'Invalid username' in rv.data
+        rv = self.login('admin', 'defaultx')
+        assert 'Invalid password' in rv.data
+
+    def test_login_logout(self):
+        rv = self.login('admin', 'default')
+        assert 'You were logged in' in rv.data
+        rv = self.logout()
+        assert 'You were logged out' in rv.data
+        rv = self.login('adminx', 'default')
+        assert 'Invalid username' in rv.data
+        rv = self.login('admin', 'defaultx')
+        assert 'Invalid password' in rv.data
+
+    def test_messages(self):
+        self.login('admin', 'default')
+        rv = self.app.post('/add', data=dict(
+            title='<Hello>',
+            text='<strong>HTML</strong> allowed here',
+            email='daisy22229999@gmail.com'
+        ), follow_redirects=True)
+        assert 'No entries here so far' not in rv.data
+        assert '&lt;Hello&gt;' in rv.data
+        assert 'daisy22229999@gmail.com' in rv.data
+        print rv.data
+
+
+    def avatar(self,email):
+        return self.app.get("http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest()+"?", follow_redirects=True)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
