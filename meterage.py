14912ce5f2321710124ddde974951b634815f796
Thomas Porcaro <tpor9095@uni.sydney.edu.au>
2015-05-07 19:06:34 +1000

    Fixed issue with entries not showing
    Of course this was a silly error: I regenerated the database, and
    there were no users in the database.

--- meterage.py
+++ meterage.py
@@ -30,6 +30,7 @@ def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
+            db.execute("insert into userPassword values ('admin','default','daisy22229999@gmail.com')")
         db.commit()
 
 
