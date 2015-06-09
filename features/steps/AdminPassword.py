8dcff223f8905f84b0fa4265d27231872939ad8a
Thomas Porcaro <tpor9095@users.noreply.github.com>
2015-06-09 17:00:02 +1000

    Fixed behave for AdminPassword feature

--- features/steps/AdminPassword.py
+++ features/steps/AdminPassword.py
@@ -1,19 +1,20 @@
 from behave import *
+
 import meterage
-from contextlib import closing
+
 
 
 #### GIVENS
 
 @given(u'admin has changed a user password')
 def step_impl(context):
-
     context.execute_steps(u'''
         given the Admin is logged in
         when the Admin goes to change password
         and admin changes a user password
         then the password successfully changed
     ''')
+
 
 #### WHENS
 
@@ -24,11 +25,13 @@ def step_impl(context):
         password='seldon'
     ), follow_redirects=True)
 
+
 # assert rv. data password sucessfully change  (refer adminpassword unit test)
 @when(u'the Admin goes to change password')
 def step_impl(context):
     context.rv = context.app.get('/change_password')
     assert context.rv.status_code != 404, "change password page does not exist"
+
 
 @when(u'admin changes a user password')
 def step_impl(context):
@@ -43,6 +46,7 @@ def step_impl(context):
 
     assert 'Successfully changed user password' in context.rv.get_data()
 
+
 @when(u'the Admin enters an invalid username')
 def step_impl(context):
     """
@@ -54,6 +58,7 @@ def step_impl(context):
         confirm_password='potter'
     ), follow_redirects=True)
 
+
 #### THENS
 
 @then(u'the password successfully changed')
@@ -61,25 +66,22 @@ def step_impl(context):
     """
     Check in the database to see that the new value for the user's password is as it should be.
     """
-    with closing(meterage.connect_db()) as db:
-        cur = db.execute('select password from userPassword where username=?', ['hari'])
-        assert len(cur.fetchall()) == 1, "username is not unique"
-        for pw in [row[0] for row in cur.fetchall()]:
-            assert pw == "potter", "Password has not been changed successfully"
+    u = meterage.User.query.filter_by(username='hari').all()
+    assert len(u) == 1, 'username is not unique'
+    assert u[0].check_password('potter'), 'Password change failed.'
+
 
 @then(u'the change fails')
 def step_impl(context):
+    assert meterage.User.query.filter_by(username='h').first() is None, \
+        "the username entered by the admin corresponds to a user in the database"
 
-    with closing(meterage.connect_db()) as db:
-        cur = db.execute('select password from userPassword where username=?', ['h'])
-        assert len(cur.fetchall()) == 0, "the username entered by the admin corresponds to a user in the database"
-
-    errorstring = "<p class=error><strong>Error:</strong>User does not exist</p>"
+    errorstring = "<strong>Error:</strong> User does not exist"
     assert errorstring in context.rv.get_data(), "the error is not being displayed"
+
 
 @then(u'user login fails')
 def step_impl(context):
-
     # logout of Admin account
     context.app.get('/logout')
 
