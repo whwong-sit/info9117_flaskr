f07568a30813c476acc99abc5e0a9ef23a86403e
Thomas Porcaro <tpor9095@users.noreply.github.com>
2015-06-09 16:54:53 +1000

    behave for AddingNewAdmins feature passing

--- features/steps/AddingNewAdmins.py
+++ features/steps/AddingNewAdmins.py
@@ -16,11 +16,13 @@ def step_impl(context):
         username='hari',
     ), follow_redirects=True)
 
+
 @when(u'the Admin enters an invalid admin name')
 def step_impl(context):
     context.rv = context.app.post('/add_new_admin', data=dict(
         username='jim'
     ), follow_redirects=True)
+
 
 @when(u'the admin is revoking privilege from a normal user')
 def step_impl(context):
@@ -28,22 +30,22 @@ def step_impl(context):
         username='test'
     ), follow_redirects=True)
 
+
 #### THENS
 
 @then(u'the user become an admin successfully')
 def step_impl(context):
-    assert 'Successfully granted admin privilege to user' in context.rv.get_data()
     """
     Check in the database to see that the new value for the user's flag_admin is as it should be.
     """
-    with closing(meterage.connect_db()) as db:
-        cur = db.execute('select flag_admin from userPassword where username=?', ['hari'])
-        row = cur.fetchone()
-        assert row[0] == 1, "User is not an admin"
+    assert 'Successfully granted admin privileges to hari' in context.rv.get_data()
+    assert meterage.User.query.filter_by(username='hari').first().admin, "User is not an admin"
+
 
 @then(u'the add fails')
 def step_impl(context):
-    assert 'User does not exist' in context.rv.get_data()
+    assert 'User jim does not exist' in context.rv.get_data()
+
 
 @then(u'the revoke fails')
 def step_impl(context):
@@ -51,10 +53,9 @@ def step_impl(context):
     """
     Check in the database to see that the value for the user's flag_admin is as it should be.
     """
-    with closing(meterage.connect_db()) as db:
-        cur = db.execute('select flag_admin from userPassword where username=?', ['test'])
-        row = cur.fetchone()
-        assert row[0] == 0, "User is an admin"
+
+    assert meterage.User.query.filter_by(username='hari').first().admin, 'user is not an admin'
+
 
 @then(u'the admin is revoking privilege from another admin')
 def step_impl(context):
@@ -62,13 +63,11 @@ def step_impl(context):
         username='spock'
     ), follow_redirects=True)
 
+
 @then(u'the revoke succeeds')
 def step_impl(context):
-    assert 'Successfully revoked admin privilege from user' in context.rv.get_data()
     """
     Check in the database to see that the new value for the user's flag_admin is as it should be.
     """
-    with closing(meterage.connect_db()) as db:
-        cur = db.execute('select flag_admin from userPassword where username=?', ['spock'])
-        row = cur.fetchone()
-        assert row[0] == 0, "User is an admin"
+    assert 'Successfully revoked admin privileges from spock' in context.rv.get_data()
+    assert not meterage.User.query.filter_by(username='spock').first().admin, 'User is an admin'
